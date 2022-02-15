c9b08984f472938fdc0fd5ee950473ff8634e9ee
Louie Cabral <louiealdrin25@gmail.com>
2022-02-15 15:51:11 +0800

    Contact Design Done

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,7 +1,56 @@
-function Contact(){
-    return(
-        <h1>Contact</h1>
-    )
+function Contact() {
+  return (
+    <>
+      <div className="contact-container">
+        <section className="app-container">
+          <form>
+          <h1>Schedule an Appointment</h1>
+            <div className="schedule-form">
+              <div className="per-info">
+                <label for="fname">Firstname:</label>
+                <input type="text" className="txt" id="fname"></input>
+                <label for="lname">Lastname:</label>
+                <input type="text" className="txt" id="lname"></input>
+              </div>
+
+              <div className="contact-info">
+                <label for="contnum">Contact Number:</label>
+                <input type="text" className="txt" id="contnum"></input>
+                <label for="email">Email Address:</label>
+                <input type="text" className="txt" id="email"></input>
+              </div>
+
+              <div className="msg-container">
+                <label for="txt-msg">Message</label>
+                <textarea name="" id="txt-msg" cols="60" rows="10"></textarea>
+              </div>
+
+              <div className="btn-con">
+               <input type="button" className="btn-submit" value="Submit" />
+              </div>
+            </div>
+          </form>
+        </section>
+
+        <section className="company-contact">
+        <div className="info">
+        <h1>Contact Information</h1>
+          <h1>Main Office</h1>
+          <p>721 Fifth Avenue Manhattan, New York</p>
+
+          <h1>Office Number</h1>
+          <p>5251-6531</p>
+
+          <h1>Office Hours</h1>
+          <p>8AM - 10PM</p>
+
+          <h1>Customer Care Hotline</h1>
+          <p>9415 - 5161</p>
+        </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
