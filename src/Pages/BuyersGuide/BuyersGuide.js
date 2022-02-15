7f7d2a858de17f067b5fa86dca0ff5d477ccb81e
Louie Cabral <louiealdrin25@gmail.com>
2022-02-15 17:27:40 +0800

    Buyers Guide Design Done

--- src/Pages/BuyersGuide/BuyersGuide.js
+++ src/Pages/BuyersGuide/BuyersGuide.js
@@ -1,6 +1,36 @@
-function BuyersGuide(){
-    return(
-        <h1>Buyers Guide</h1>
-    )
+import step1 from "Assets/images/contract2.jpg";
+import step2 from "Assets/images/realtor.jpg";
+import step3 from "Assets/images/contract2.jpg";
+import step4 from "Assets/images/sale-sign.jpg";
+import step5 from "Assets/images/property.jpg";
+function BuyersGuide() {
+  return (
+    <div className="buyers-container">
+         <div className="box">
+             <img src={step1} alt="tite" className="img-step"></img>
+             <p>Schedule an Appointment with our Business partner</p>
+         </div>
+
+         <div className="box">
+             <img src={step2} alt="tite" className="img-step"></img>
+             <p>Tour around the house</p>
+         </div>
+
+         <div className="box">
+             <img src={step3} alt="tite" className="img-step"></img>
+             <p>Fill up Application for reservation of unit</p>
+         </div>
+
+         <div className="box">
+             <img src={step4} alt="tite" className="img-step"></img>
+             <p>Confirmation of buying</p>
+         </div>
+
+         <div className="box">
+             <img src={step5} alt="tite" className="img-step"></img>
+             <p>Turnover of property</p>
+         </div>
+      </div>
+  );
 }
 export default BuyersGuide;
---
+++ src/Pages/HouseData/buyerGuide.js
@@ -0,0 +1,33 @@
+import step1 from "Assets/images/contract2.jpg";
+import step2 from "Assets/images/realtor.jpg";
+import step3 from "Assets/images/contract2.jpg";
+import step4 from "Assets/images/sale-sign.jpg";
+import step5 from "Assets/images/property.jpg";
+export const buyersGuide = [
+  {
+    buyStep: step1,
+    alt:"Contract Signing",
+    description:"Schedule an Appointment with our Business partner"
+  },
+  {
+    buyStep: step2,
+    alt:"Realtor House Tour",
+    description:"Tour around the house"
+  },
+  {
+    buyStep: step3,
+    alt:"Contract Signing",
+    description:"Fill up Application for reservation of unit"
+
+  },
+  {
+    buyStep: step4,
+    alt:"For Sale Sign Sold",
+    description:"Confirmation of buying"
+  },
+  {
+    buyStep: step5,
+    alt:"Toy House",
+    description:"Turnover of property"
+  },
+];
