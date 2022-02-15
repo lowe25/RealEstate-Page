544992a29bb3bc260b8ddf729c967dde65f50131
Louie Cabral <louiealdrin25@gmail.com>
2022-02-15 13:39:14 +0800

    Properties Design Done

---
+++ src/Pages/HouseData/houseData.js
@@ -0,0 +1,116 @@
+import siouxHouse from "Assets/images/Sioux.jpg";
+import calgaryHouse from "Assets/images/Calgary.jpg";
+import idahoHouse from "Assets/images/Idaho.jpg";
+import mediterHouse from "Assets/images/Mediterrian.jpg";
+import atlaticHouse from "Assets/images/atlantic.jpg";
+import stCloudHouse from "Assets/images/StCloud.jpg";
+export const houseData = [
+    {
+        imgHouse:siouxHouse,
+        houseName:"Sioux",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+    {
+        imgHouse:calgaryHouse,
+        houseName:"Calgary",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+    {
+        imgHouse:idahoHouse,
+        houseName:"Idaho",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+    {
+        imgHouse:mediterHouse,
+        houseName:"Mediterranean",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+    {
+        imgHouse:atlaticHouse,
+        houseName:"Atlantic",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+    {
+        imgHouse:stCloudHouse,
+        houseName:"St Cloud",
+        housePrice:"$5,000",
+        floorArea:"166 sqm",
+        lotArea:"143 sqm",
+        //House Info
+        houseBath:"2",
+        houseGarage:"1",
+        houseBedrooms:"3",
+        //Features
+        feature1:"Living Area",
+        feature2:"Dining Area",
+        feature3:"Kitchen",
+        feature4:"Balcony",
+        feature5:"Porch",
+        feature6:"1 Garage"
+    },
+]
--- src/Pages/Properties/Properties.js
+++ src/Pages/Properties/Properties.js
@@ -1,5 +1,32 @@
+import { houseData } from "../HouseData/houseData";
 function Properties() {
-  return <h1>Properties</h1>;
+  return (
+    <>
+      <div className="properties-container">
+        <div className="house-list">
+        <section className="property-list">
+        <h1>House And Lot</h1>
+        <div className="box-con">
+          {houseData.map((item, index) => {
+            return (
+              <>
+                <div className="box">
+                  <img
+                    src={item.imgHouse}
+                    className="housePicture"
+                    alt="House and Lot"
+                  ></img>
+                  <p className="houseName">{item.houseName}</p>
+                </div>
+              </>
+            );
+          })}
+          </div>
+        </section>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Properties;
