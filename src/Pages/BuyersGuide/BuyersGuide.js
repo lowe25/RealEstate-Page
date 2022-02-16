6cf624a6592e055880e71f39fd6e8b2e33d41c7f
Louie Cabral <louiealdrin25@gmail.com>
2022-02-16 14:16:56 +0800

    Buyer Guide Design Done

--- src/Pages/BuyersGuide/BuyersGuide.js
+++ src/Pages/BuyersGuide/BuyersGuide.js
@@ -6,31 +6,80 @@ import step5 from "Assets/images/property.jpg";
 function BuyersGuide() {
   return (
     <div className="buyers-container">
-         <div className="box">
-             <img src={step1} alt="tite" className="img-step"></img>
-             <p>Schedule an Appointment with our Business partner</p>
-         </div>
+      <section className="steps-container">
+        <h1>Buyers Guide</h1>
+        <div className="steps">
+          <div className="box">
+            <img src={step1} alt="Contract Signing" className="img-step"></img>
+            <p>Schedule an Appointment with our Business partner</p>
+          </div>
 
-         <div className="box">
-             <img src={step2} alt="tite" className="img-step"></img>
-             <p>Tour around the house</p>
-         </div>
+          <div className="box">
+            <img
+              src={step2}
+              alt="Realtor House Tour"
+              className="img-step"
+            ></img>
+            <p>Tour around the house</p>
+          </div>
 
-         <div className="box">
-             <img src={step3} alt="tite" className="img-step"></img>
-             <p>Fill up Application for reservation of unit</p>
-         </div>
+          <div className="box">
+            <img src={step3} alt="Contract Signing" className="img-step"></img>
+            <p>Fill up Application for reservation of unit</p>
+          </div>
 
-         <div className="box">
-             <img src={step4} alt="tite" className="img-step"></img>
-             <p>Confirmation of buying</p>
-         </div>
+          <div className="box">
+            <img
+              src={step4}
+              alt="For Sale Sign Sold"
+              className="img-step"
+            ></img>
+            <p>Confirmation of buying</p>
+          </div>
 
-         <div className="box">
-             <img src={step5} alt="tite" className="img-step"></img>
-             <p>Turnover of property</p>
-         </div>
+          <div className="box">
+            <img src={step5} alt="Toy House" className="img-step"></img>
+            <p>Turnover of property</p>
+          </div>
+        </div>
+      </section>
+      <div className="buyers-btn">
+        <button type="button" className="btn-sched" value="Search">
+          Schedule Appointment
+        </button>
       </div>
+      {/** */}
+      <div className="buyers-process">
+        <section className="reservation">
+          <h1>Reservation Process:</h1>
+          <ol>
+            <li>
+              Choose your preferred project location and the type of unit.
+            </li>
+            <li>
+              Request for a sample computation of the unit through our
+              accredited seller to get an idea on the financing
+            </li>
+            <li>
+              Fill up completely and sign the Reservation Agreement form and
+              prepare 2 valid ID's.
+            </li>
+            <li>Pay the Reservation Fee</li>
+          </ol>
+        </section>
+
+        <section className="documents">
+          <h1>Documentary Requirements</h1>
+          <ul>
+            <li>Final Computation Sheet</li>
+            <li>Credit Approval</li>
+            <li>Reservation Fee and Post Dated Checks for Down Payment</li>
+            <li>2 Valid Government Issued ID</li>
+            <li>Bank Guarantee or Guarantee Check</li>
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 }
 export default BuyersGuide;
