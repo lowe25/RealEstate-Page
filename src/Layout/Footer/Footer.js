3456db5fa9cb3481c94c83c4240e18afef49e984
Louie Cabral <louiealdrin25@gmail.com>
2022-02-22 14:33:29 +0800

    Homepage Media Queries Added

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,48 +1,10 @@
+//import React, { useState } from "react";
+//import { FooterD } from "./FooterData";
 function Footer() {
+ 
   return (
     <>
-      <footer className="footer">
-        <section className="comp-desc">
-          <h1>Richmond Properties</h1>
-          <p>
-            Celebrate life everyday with Avida, and experience a lifestyle{" "}
-            <br />
-            that is easy, distinct, and enriching. With over three decades
-            <br />
-            of providing sure, secure, and sensible homes in sustainable <br />
-            communities nationwide, Avida has become the developer of <br />
-            choice among the rising middle class making dream spaces a <br />
-            reality while offering more than the essentials.
-          </p>
-        </section>
-
-        <section className="contacts">
-          <ul className="comp-info">
-          <li className="bold">Company Contacts</li>
-            <li>Richmond Properties</li>
-            <li>richmondprop@gmail.com</li>
-            <li>(02) 4 534-9914</li>
-            <li>(63) 941 624 8951</li>
-          </ul>
-
-          <ul className="comp-social">
-              <li className="bold">Socials</li>
-              <li><i className="fa-brands fa-facebook-f"></i></li>
-              <li><i className="fa-brands fa-youtube"></i></li>
-              <li><i className="fa-brands fa-instagram"></i></li>
-          </ul>
-
-          <ul className="comp-links">
-              <li className="bold">Links</li>
-              <li>About</li>
-              <li>Properties</li>
-              <li>Contacs</li>
-          </ul>
-        </section>
-      </footer>
-      <div className="copyright">
-          <p>Richmond Properties Allrights Reserved 2022</p>
-      </div>
+     
     </>
   );
 }
---
+++ src/Layout/Footer/FooterData.js
@@ -0,0 +1,29 @@
+export const FooterD = [
+    {
+        comp:"Richmond Properties",
+        compAbout:`
+        Celebrate life everyday w--enriching. With over three decades 
+        of providing sure, secure, and sensible homes in sustainable 
+        communities nationwide, Avida has become the developer of
+        choice among the rising middle class making dream spaces a 
+        reality while offering more than the essentials.`
+    },
+    {
+        comp:"Richmond Properties",
+        compSocial1:"richmondprop@gmail.com",
+        compSocial2:"(02) 4 534-9914",
+        compSocial3:"(63) 941 624 8951"
+    },
+    {
+        comp:"Socials",
+        compSocial1:"Facebook",
+        compSocial2:"Youtube",
+        compSocial3:"Instagram",
+    },
+    {
+        comp:"Links",
+        compSocial1:"About",
+        compSocial2:"Properties",
+        compSocial3:"Contact",
+    }
+]
